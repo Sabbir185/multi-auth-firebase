bac0ce78d0bc2fa4dcffdf622251bccec69123eb
Sabbir Ahmmed <sabbir.cse@yahoo.com>
2021-03-17 16:28:15 +0600

    github added

--- src/App.js
+++ src/App.js
@@ -14,9 +14,11 @@ if (!firebase.apps.length) {
 function App() {
   const [user, setUser] = useState({});
   const [fbUser, setFbUser] = useState({});
+  const [ghUser, setGhUser] = useState({});
 
   var googleProvider = new firebase.auth.GoogleAuthProvider();
   var fbProvider = new firebase.auth.FacebookAuthProvider();
+  var ghProvider = new firebase.auth.GithubAuthProvider();
 
   // google
   const googleSignInHandler = () => {
@@ -61,16 +63,43 @@ function App() {
     });
   }
 
+
+  // github
+  const gitSignInHandler = () => {
+    firebase
+    .auth()
+    .signInWithPopup(ghProvider)
+    .then((result) => {
+      /** @type {firebase.auth.OAuthCredential} */
+      var credential = result.credential;
+      var user = result.user;
+      var accessToken = credential.accessToken;
+      console.log('github user ',user);
+      setGhUser(user);
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      var email = error.email;
+      var credential = error.credential;
+      console.log(errorCode, errorMessage);
+    });
+  }
+
   return (
     <div className='App'>
       
       <button onClick={googleSignInHandler} className='btn btn-primary mt-5 mb-3'>google</button>
-      <h3>Email : {user.email}</h3>
-      <img src={user.photoURL} alt=""/>
+      {/* <h3>Email : {user.email}</h3>
+      <img src={user.photoURL} alt=""/> */}
 
       <button onClick={fbSignInHandler} className='btn btn-primary mt-5 mb-3'>Facebook</button>
-      <h3>Display Name : {fbUser.displayName}</h3>
-      <img src={fbUser.photoURL} alt=""/>
+      {/* <h3>Display Name : {fbUser.displayName}</h3>
+      <img src={fbUser.photoURL} alt=""/> */}
+
+      <button onClick={gitSignInHandler} className='btn btn-primary mt-5 mb-3'>Github</button>
+      <h3>Display Name : {ghUser.displayName}</h3>
+      <img src={ghUser.photoURL} alt=""/>
   
     </div>
   );
